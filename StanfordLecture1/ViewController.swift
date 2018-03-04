78d218a5114793e3a051cf36255c7a1582cd4576
STANISLAV <minolin27@gmail.com>
2018-03-04 14:09:28 +0300

    Initial Commit

---
+++ StanfordLecture1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StanfordLecture1
+//
+//  Created by Станислав Коцарь on 04.03.2018.
+//  Copyright © 2018 Станислав Коцарь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
